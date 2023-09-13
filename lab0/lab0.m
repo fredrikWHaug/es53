523b4bd657ceba1f4abf24f7b9db6d6a08dce448
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-09-13 12:54:23 -0400

    Update lab0.m

--- lab0/lab0.m
+++ lab0/lab0.m
@@ -1,36 +1,25 @@
-%%
-% (1) new script
+%% Header
 % Filename: lab0
 % Author: Fredrik Willumsen Haug
 % Created: September 8th, 2023
 % Description: MATLAB intro
 %%
 
-%%
-% (2) vector and matrix declaration and operation
-
-% multiplication and division with transposition
+% vector and matrix declaration and operation (2)
+%% 2.1
 mat1 = 1:5;
-
 mat2 = [11:15; 21:25];
-
 transposition_product = mat1 * mat2';
-
 transposition_quotient = mat1 / mat2; % denominater gets transposed implicitly
 
-% elementwise operations
 sum = mat1 + mat2;
 difference = mat1 - mat2;
 product = mat1 .* mat2;
 quotient = mat1 ./ mat2;
-
 %%
 
-%%
-% (3) conditionals
-
-% 3.1 - else if with numerical output
-% random number with seed 1
+% conditionals (3)
+%% 3.1
 rng(1);
 random_int = randi(20);
 
@@ -41,9 +30,9 @@ elseif random_int < 10
 else 
     DISP(2)
 end
+%%
 
-% 3.2 - comparison with string output
-% random numers assigned to A and with seed 1
+%% 3.2
 A = randi(10);
 B = randi(10);
 
@@ -54,12 +43,10 @@ elseif A > B
 else
     DISP("B is greater than A")
 end
-
 %%
 
-%%
-% (4) loops
-% 4.1 - while loop adding all odd numbers from 1 to 3553
+% loops (4)
+%% 4.1
 index = 1;
 odd_sum = 0;
 while index <= 3553
@@ -67,8 +54,9 @@ odd_sum = odd_sum + index;
 index = index + 2;
 end
 fprintf("Sum of odd numbers: %d", odd_sum);
+%%
 
-% 4.2 while loop adding all even numbers from 0 to 3554
+%% 4.2
 index = 0;
 even_sum = 0;
 while index <= 3554
@@ -80,14 +68,11 @@ fprintf("Sum of even numbers: %d", even_sum);
 %%
 
 
-% (5) searching and indexing
+% searching and indexing (5)
 %% 5.1
 mat = rand(100);
-
 condition = mat < 0.25;
-
-condition_array = mat(condition); % array containing numbers fulfilling 
-                                  % condition
+condition_array = mat(condition);                                % condition
 %%
 
 %% 5.2
@@ -100,12 +85,11 @@ conditional_mat = mat(condition);
 mat  = randi([0, 10], 1, 100);
 squared_mat = mat.^2;
 k = find(squared_mat > 50);
-
 segregated_mat = mat(k);
 mat_size = size(segregated_mat);
 %%
 
-%(6) greater than implementation
+% greater than implementation (6)
 %% 6.1
 
 yVector = randi([0, 10], 1, 100);
@@ -126,13 +110,11 @@ newYVector2 = newYVector * -1;
 
 %%
 
-%%
-% graph
-% functions and documentation
+% graph (7)
+%% 7.1
 t = 0:1/4000:3-1/4000;
 q = chirp(t-3,4,1/2,6,'quadratic',100,'convex').*exp(-4*(t-1).^2);
 plot(t,q)
-
 [up, lo] = envelope(q);
 hold on
 plot(t, up, 'LineWidth',5, Color="red");
