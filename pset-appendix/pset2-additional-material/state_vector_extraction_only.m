68442f3e11c3e62cce02601bc9262df1264efa1e
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-09-24 16:46:13 -0400

    Create state_vector_extraction_only.m

---
+++ pset-appendix/pset2-additional-material/state_vector_extraction_only.m
@@ -0,0 +1,14 @@
+% % subplot 2
+% 
+% gk_max = 36;
+% gna_max = 120;
+% 
+% sodium_conductance = (y(:, 2).^3) .* y(:, 3) .* gna_max;
+% potassium_conductance = (y(:, 4)).^4;
+% display(potassium_conductance);
+% 
+% subplot(3, 1, 2);
+% hold on
+% plot(sodium_conductance);
+% plot(potassium_conductance);
+% hold off
