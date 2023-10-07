001df577992e2cc45836e85894b643e0d5625e2b
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-10-07 13:44:12 -0400

    Create figure1.m

---
+++ lab3/figure1.m
@@ -0,0 +1,14 @@
+
+% loading through labchart because load gave positive or local value error
+relax = data((datastart(1,1)+comtickpos(2)):datastart(1,1)+comtickpos(3));
+moving = data((datastart(1,1)+comtickpos(3)):datastart(1,1)+comtickpos(4));
+
+% defining time vector to get seconds for moving
+time_relax = (1 :length(relax)) / 1000;
+time_moving = (1 : length(moving)) / 1000;
+
+figure(1);
+hold on
+plot(time_relax, relax);
+plot(time_moving, moving);
+hold off
