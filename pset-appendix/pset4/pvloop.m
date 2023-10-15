985a66e533b2141dcf0fbb2a3474af3a8f13512f
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-10-15 16:12:28 -0400

    Update pvloop.m

--- pset-appendix/pset4/pvloop.m
+++ pset-appendix/pset4/pvloop.m
@@ -1,10 +1,11 @@
-
 % plotting pressure vs volume from PVloop.mat
 pv_loop = load('PVloop.mat');
 
 pv_volume = [pv_loop.PVloop(1,:) 60];
 pv_pressure = [pv_loop.PVloop(2,:) 3.5];
 
-plot(PV_volume, PV_pressure);
+stroke_work = polyarea(pv_volume, pv_pressure);
+
+plot(pv_volume, pv_pressure);
 xlabel('Volume (ml)');
 ylabel('Pressure (mmHG)');
