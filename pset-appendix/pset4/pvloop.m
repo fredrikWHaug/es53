89543e68652264a80aa80cd5d4476b2a1433d85e
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-10-17 21:42:49 -0400

    Update pvloop.m

--- pset-appendix/pset4/pvloop.m
+++ pset-appendix/pset4/pvloop.m
@@ -1,11 +1,14 @@
-% plotting pressure vs volume from PVloop.mat
+% loading data as struct
 pv_loop = load('PVloop.mat');
 
+% extracting volume and pressure respectively
 pv_volume = [pv_loop.PVloop(1,:) 60];
 pv_pressure = [pv_loop.PVloop(2,:) 3.5];
 
+% taking integral of pressure volume loop with polyarea
 stroke_work = polyarea(pv_volume, pv_pressure);
 
+% plot with labeled and ajusted axes
 plot(pv_volume, pv_pressure);
 xlabel('Volume (ml)');
 ylabel('Pressure (mmHG)');
