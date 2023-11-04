217052839e368904c43de897bfaf1236fadeec22
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-11-04 16:50:29 -0400

    Update es53_lab3.m

--- lab3/src/es53_lab3.m
+++ lab3/src/es53_lab3.m
@@ -7,21 +7,19 @@
 
 %% Figure 1
 % load data from all subjects
-subject1 = load('Lab3_Ex2_Sub1.mat');
-subject2 = load('Lab3_Ex2_Sub2.mat');
-subject3 = load('Lab3_Ex2_Sub3.mat');
+ex1 = load('Lab3_Ex1_Sub1.mat');
 
 % extracting samplerate
 sr = ex1.samplerate(1);
 
 % data extraction for sitting still and defining time vector
 % from start to 16 seconds determined empirically 
-still = ex1.data(ex1.datastart(1, 1) : ex1.datastart(1, 1) + 16 * sr);
+still = ex1.data(ex1.datastart(1, 1) : ex1.datastart(1, 1) + 10 * sr);
 time_still = (0 : length(still) - 1);
 
 % data extraction for arm movement
 % from comment 2 (arm movement start)to 16 seconds to allign with still
-arm_movement = ex1.data(ex1.datastart(1, 1) + ex1.com(2, 3) : ex1.datastart(1, 1) + ex1.com(2,3) + 16 * sr);
+arm_movement = ex1.data(ex1.datastart(1, 1) + ex1.com(2, 3) : ex1.datastart(1, 1) + ex1.com(2,3) + 10 * sr);
 time_arm_movement = (0 : length(arm_movement) - 1);
 
 % plot for displaying data artifacts due to movement
