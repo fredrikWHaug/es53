171097148abee6da76e3a1bc1bec38d872a5ffdf
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-11-10 16:22:33 -0500

    Create es53_lab6.m

---
+++ lab6/es53_lab6.m
@@ -0,0 +1,107 @@
+%% ENG-SCI 53 Lab 5
+% Filename: es53_lab5.m
+% Author: Chris Kim, Benjamin Brenner, Fredrik Willumsen Haug
+% Created: November 4th
+% Description: ECG and Blood Volume Pulse
+%%
+
+%% Figure 1
+% load data from all group subjects
+benji = load('Lab6_Benji_Ex1-3.mat');
+chris = load('Lab6_Chris_Ex1-3.mat');
+fredrik = load('Lab6_Fredrik_Ex1-3.mat');
+
+sample_rate = benji.samplerate(1);
+
+% benji
+% 60 seconds normal breathing
+benji_normal_breathing = benji.data(benji.datastart(1, 1) : benji.datastart(1, 1) + (60 * sample_rate));
+time_benji_normal = (0 : length(benji_normal_breathing) - 1) / sample_rate;
+
+% 60 seconds inhale hold
+benji_inhale_hold = benji.data(benji.com(2, 3) : benji.com(3, 3));
+time_benji_inhale_hold = (0 : length(benji_inhale_hold) - 1) / sample_rate;
+
+% 60 seconds exhale hold
+benji_exhale_hold = benji.data(benji.com(4, 3) - (10 * sample_rate) : benji.com(5, 3) + (25 * sample_rate));
+time_benji_exhale_hold = (0 : length(benji_exhale_hold) - 1) / sample_rate;
+
+% chris
+% 60 seconds normal breathing
+chris_normal_breathing = chris.data(chris.datastart(1, 1) : chris.datastart(1, 1) + (60 * sample_rate));
+time_chris_normal = (0 : length(chris_normal_breathing) - 1) / sample_rate;
+
+% 60 seconds inhale hold
+chris_inhale_hold = chris.data(chris.com(2, 3) : chris.com(3, 3) - (100 * sample_rate));
+time_chris_inhale_hold = (0 : length(chris_inhale_hold) - 1) / sample_rate;
+
+% 60 seconds exhale hold
+chris_exhale_hold = chris.data(chris.com(4, 3) - (10 * sample_rate) : chris.com(5, 3) - (40 * sample_rate));
+time_chris_exhale_hold = (0 : length(chris_exhale_hold) - 1) / sample_rate;
+
+% fredrik
+% 60 seconds normal breathing
+fredrik_normal_breathing = fredrik.data(fredrik.datastart(1, 1) : fredrik.datastart(1, 1) + (60 * sample_rate));
+time_fredrik_normal = (0 : length(fredrik_normal_breathing) - 1) / sample_rate;
+
+% 60 seconds inhale hold
+fredrik_inhale_hold = fredrik.data(fredrik.com(1, 3) : fredrik.com(2, 3) - (30 * sample_rate));
+time_fredrik_inhale_hold = (0 : length(fredrik_inhale_hold) - 1) / sample_rate;
+
+% 60 seconds exhale hold
+fredrik_exhale_hold = fredrik.data(fredrik.com(3, 3) - (10 * sample_rate) : fredrik.com(4, 3));
+time_fredrik_exhale_hold = (0 : length(fredrik_exhale_hold) - 1) / sample_rate;
+
+% all nine plots in order of person
+% benji plots (column 1)
+subplot(3, 3, 1);
+plot(time_benji_normal, benji_normal_breathing);
+xlabel('Time (s)');
+ylabel('Voltage (mV)');
+
+subplot(3, 3, 4);
+plot(time_benji_inhale_hold, benji_inhale_hold);
+xlabel('Time (s)');
+ylabel('Voltage (mV)');
+
+subplot(3, 3, 7);
+plot(time_benji_exhale_hold, benji_exhale_hold);
+xlabel('Time (s)');
+ylabel('Voltage (mV)');
+
+% chris plots (column 2)
+subplot(3, 3, 2);
+plot(time_chris_normal, chris_normal_breathing);
+xlabel('Time (s)');
+ylabel('Voltage (mV)');
+
+subplot(3, 3, 5);
+plot(time_chris_inhale_hold, chris_inhale_hold);
+xlabel('Time (s)');
+ylabel('Voltage (mV)');
+
+subplot(3, 3, 8);
+plot(time_chris_exhale_hold, chris_exhale_hold);
+xlabel('Time (s)');
+ylabel('Voltage (mV)');
+
+% fredrik plots (column 3)
+subplot(3, 3, 3);
+plot(time_fredrik_normal, fredrik_normal_breathing);
+xlabel('Time (s)');
+ylabel('Voltage (mV)');
+
+subplot(3, 3, 6);
+plot(time_fredrik_inhale_hold, fredrik_inhale_hold);
+xlabel('Time (s)');
+ylabel('Voltage (mV)');
+
+subplot(3, 3, 9);
+plot(time_fredrik_exhale_hold, fredrik_exhale_hold);
+xlabel('Time (s)');
+ylabel('Voltage (mV)');
+%%
+
+%% Average Breathing Rate
+
+%%
