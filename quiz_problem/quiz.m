30e567e1b2548d31c8f82fc5791fba386689375a
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-12-04 14:21:28 -0500

    Update quiz.m

--- quiz_problem/quiz.m
+++ quiz_problem/quiz.m
@@ -1,4 +1,3 @@
-
 % load data
 subject = load('respiratorydata.mat');
 sample_rate = subject.samplerate(1);
@@ -11,9 +10,29 @@ subject_pulse = subject.data(subject.datastart(2, 1) : subject.datastart(2, 1) +
 time_breath = (0 : length(subject_breath) - 1) / sample_rate;
 time_pulse = (0 : length(subject_pulse) - 1) / sample_rate;
 
+% subject peak finding
+e_subject = subject_breath;
+de_subject = diff(e_subject); % derivative of this vector
+threshold = 10; % empirical peak treshold
+
+% plot
+time_subject_breath = (1:length(e_subject))/sample_rate;
+%figure(2);
+%plot(time_subject_breath, e_subject);
+hold on
+MPH = threshold;
+MPD = 2 * sample_rate;
+[~,subject_breath_peaks] = findpeaks(e_subject,'MinPeakHeight',MPH,'MinPeakDistance',MPD);
+%plot(time_subject_breath(subject_breath_peaks), e_subject(subject_breath_peaks),'ro');
+title('Subject pulse normal breathing');
+hold off
+
 figure(1);
 subplot(2, 1, 1);
 plot(time_breath, subject_breath, 'black');
+hold on
+plot(time_subject_breath(subject_breath_peaks), e_subject(subject_breath_peaks),'ro');
+hold off
 xlabel('Time (s)');
 ylabel('Lung expansion (a.u)');
 
@@ -21,13 +40,3 @@ subplot(2, 1, 2);
 plot(time_pulse, subject_pulse, 'black');
 xlabel('Time (s)');
 ylabel('Pulse (a.u)');
-
-% peak finding
-e_subject = subject_breath / max(benji_normal_breathing); % normalize
-de_benji = diff(e_benji); % derivative of this vector
-threshold = 0.8; % treshold due to normalization
-MPH = threshold;
-MPD = 5 * sample_rate;
-[~,benji_breath_peaks] = findpeaks(e_benji,'MinPeakHeight',MPH,'MinPeakDistance',MPD);
-
-
