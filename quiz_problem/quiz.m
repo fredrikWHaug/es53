5f1d0d046f415051059ee6cbed466248fc125056
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-12-04 19:05:13 -0500

    Update quiz.m

--- quiz_problem/quiz.m
+++ quiz_problem/quiz.m
@@ -17,35 +17,41 @@ time_pulse = (0 : length(subject_pulse) - 1) / sample_rate;
 %% Peaks
 % peak finding part 1
 peak_1_data = subject.data(subject.datastart(1, 1) : subject.datastart(1, 1) + 120 * sample_rate);
-e_1 = peak_1_data;
-de_1 = diff(e_1); % derivative of this vector
+p_1 = peak_1_data;
 threshold = 10.5; % visual peak treshold
-time_1 = (1:length(e_1)) / sample_rate;
+time_p1 = (1:length(p_1)) / sample_rate;
 MPH = threshold;
 MPD = 2 * sample_rate;
-[~,peaks_1] = findpeaks(e_1,'MinPeakHeight',MPH,'MinPeakDistance',MPD);
+[~,peaks_1] = findpeaks(p_1,'MinPeakHeight',MPH,'MinPeakDistance',MPD);
 
 % peak finding part 2
 peak_2_data = subject.data(subject.datastart(1, 1) + (168 * sample_rate) : subject.datastart(1, 1) + (200 * sample_rate));
-e_2 = peak_2_data;
-de_2 = diff(e_2); % derivative of this vector
+p_2 = peak_2_data;
 threshold = 13; % visual peak treshold
-time_2 = (1:length(e_2)) / sample_rate;
+time_p2 = (1:length(p_2)) / sample_rate;
 MPH = threshold;
 MPD = 3 * sample_rate;
-[~,peaks_2] = findpeaks(e_2,'MinPeakHeight',MPH,'MinPeakDistance',MPD);
+[~,peaks_2] = findpeaks(p_2,'MinPeakHeight',MPH,'MinPeakDistance',MPD);
 %%
 
 %% Troughs
 % trough finding part 1
 trough_1_data = peak_1_data * -1;
-e_trough = trough_1_data;
-de_trough = diff(e_trough); % derivative of this vector
+t_1 = trough_1_data;
 threshold = -10.5; % visual trough (peak for inverted) treshold  
-time_trough = (1:length(e_trough)) / sample_rate;
+time_t1 = (1:length(t_1)) / sample_rate;
 MPH = threshold;
 MPD = 2 * sample_rate;
-[~, trough_peaks] = findpeaks(e_trough,'MinPeakHeight',MPH,'MinPeakDistance',MPD);
+[~, troughs_1] = findpeaks(t_1,'MinPeakHeight',MPH,'MinPeakDistance',MPD);
+
+% trough finding part 2
+trough_2_data = peak_2_data * -1;
+t_2 = trough_2_data;
+threshold = -20; % visual trough (peak for inverted) treshold  
+time_t2 = (1:length(t_2)) / sample_rate;
+MPH = threshold;
+MPD = 2 * sample_rate;
+[~, troughs_2] = findpeaks(t_2,'MinPeakHeight',MPH,'MinPeakDistance',MPD);
 %%
 
 %% Plot
@@ -54,12 +60,14 @@ figure(1);
 subplot(2, 1, 1);
 plot(time_breath, subject_breath, 'black');
 hold on
-plot(time_1(peaks_1), e_1(peaks_1), 'ro');
+plot(time_p1(peaks_1), p_1(peaks_1), 'ro');
 plot(123.08, 35.2746, 'ro');
 plot(163.17, 28.1866, 'ro');
-plot(168+(time_2(peaks_2)), e_2(peaks_2), 'ro');
-plot(time_trough(trough_peaks), -1*e_trough(trough_peaks),'c*');
+plot(168 + (time_p2(peaks_2)), p_2(peaks_2), 'ro');
+
+plot(time_t1(troughs_1), -1*t_1(troughs_1),'c*');
 plot(150.86, 26.4678, 'c*');
+plot(168 + (time_t2(troughs_2)), -1*t_2(troughs_2), 'c*');
 hold off
 xlabel('Time (s)');
 ylabel('Lung expansion (a.u)');
